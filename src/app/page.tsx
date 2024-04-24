cddb682a4ed898e828a803fade231c759cb44941
Luyang Wang <lnkkerst04@gmail.com>
2024-04-24 17:56:39 +0900

    feat: adjust home content padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeMd from "@/markdown/home.mdx";
 
 export default function Home() {
   return (
-    <div className="prose max-w-none">
+    <div className="prose max-w-none px-6">
       <HomeMd></HomeMd>
     </div>
   );
