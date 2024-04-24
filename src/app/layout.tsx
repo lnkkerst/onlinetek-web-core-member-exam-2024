1c44f3b94636c86d0e6588e0a8fcd9aae6e2718c
lnkkerst <lnkkerst04@gmail.com>
2024-04-24 21:41:51 +0800

    feat: update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Web",
+  title: "0nlineTek Web 2024 年度核心成员考核",
 };
 
 export default function RootLayout({
